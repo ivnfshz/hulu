54aeaad5b10b163738d60e3266cc6b80e7436bc3
ivnfshz12 <jivanfloresh@gmail.com>
2018-06-20 22:00:08 -0500

    Initial commit of the project

---
+++ src/main/java/com/hulu/excersice/binarytree/BinaryTreeApplication.java
@@ -0,0 +1,12 @@
+package com.hulu.excersice.binarytree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinaryTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinaryTreeApplication.class, args);
+	}
+}
