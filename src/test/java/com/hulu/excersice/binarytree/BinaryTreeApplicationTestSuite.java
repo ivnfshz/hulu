7a3763968d6c6a28fab9e01f8a891e42f66b87f2
ivnfshz12 <jivanfloresh@gmail.com>
2018-06-22 01:37:23 -0500

    add class

--- src/test/java/com/hulu/excersice/binarytree/BinaryTreeApplicationTestSuite.java
+++ src/test/java/com/hulu/excersice/binarytree/BinaryTreeApplicationTestSuite.java
@@ -4,11 +4,13 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import com.hulu.excersice.binarytree.repository.BinaryTreeRepositoryTests;
 import com.hulu.excersice.binarytree.service.BinaryTreeServiceTests;
 
 @RunWith(Suite.class)
 @SuiteClasses({ 
-	BinaryTreeServiceTests.class
+	BinaryTreeServiceTests.class,
+	BinaryTreeRepositoryTests.class
 	})
 public class BinaryTreeApplicationTestSuite {
 
