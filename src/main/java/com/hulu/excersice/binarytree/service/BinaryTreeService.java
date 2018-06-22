0ac7654ecad2ba530c5d47b286c9f3f455e3710c
ivnfshz12 <jivanfloresh@gmail.com>
2018-06-21 20:49:17 -0500

    prints binary tree

--- src/main/java/com/hulu/excersice/binarytree/service/BinaryTreeService.java
+++ src/main/java/com/hulu/excersice/binarytree/service/BinaryTreeService.java
@@ -1,6 +1,7 @@
 package com.hulu.excersice.binarytree.service;
 
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeMap;
 
@@ -13,25 +14,33 @@ import com.hulu.excersice.binarytree.repository.BinaryTreeRepository;
 @Service
 public class BinaryTreeService {
 
+	private Scanner scanner;
 	private LinkedList<Node> root = new LinkedList<Node>();
 	private TreeMap<Integer, Integer> nodes = new TreeMap<Integer, Integer>();
-	
+
 	@Autowired
 	private BinaryTreeRepository binaryTreeRepository;
 
-
 	public void printBinaryTree() {
-		
+		System.out.println("====================================");
+		System.out.println("This is the binary tree: ");
+		traverseTree(this.binaryTreeRepository.getBinaryTree());
+		for (Map.Entry<Integer, Integer> entry : this.nodes.entrySet()) {
+			Integer key = entry.getKey();
+			Integer value = entry.getValue();
+			System.out.println("Level: " + value + " Node: " + key);
+		}
+		System.out.println("====================================");
 	}
-	
-	public void guessCousins(){
+
+	public void guessCousins() {
 		System.out.println("Enter your username: ");
-		Scanner scanner = new Scanner(System.in);
+		scanner = new Scanner(System.in);
 		String username = scanner.nextLine();
 		System.out.println("Your username is " + username);
 	}
 
-	public boolean isCousins(int a, int b) {
+	private boolean isCousins(int a, int b) {
 		traverseTree(this.binaryTreeRepository.getBinaryTree());
 		if (this.nodes.get(a) == this.nodes.get(b)
 				&& BinaryTreeRepository.parent.get(a) != BinaryTreeRepository.parent.get(b))
