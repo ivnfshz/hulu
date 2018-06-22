4b23620918c0bb9e0e017ae40d4e8f035908c2a6
ivnfshz12 <jivanfloresh@gmail.com>
2018-06-22 00:27:17 -0500

    add test for configuration environment.

---
+++ src/test/java/com/hulu/excersice/binarytree/config/PropertyTestConfiguration.java
@@ -0,0 +1,21 @@
+package com.hulu.excersice.binarytree.config;
+
+import java.io.IOException;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+@Configuration
+public class PropertyTestConfiguration {
+	
+	@Bean
+	public PropertyPlaceholderConfigurer propertyPlaceholderConfigurer() throws IOException {
+		final PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
+		ppc.setLocations(ArrayUtils.addAll(new PathMatchingResourcePatternResolver().getResources("classpath*:nodes")));
+
+		return ppc;
+	}
+}
---
+++ src/test/java/com/hulu/excersice/binarytree/config/PropertyTests.java
@@ -0,0 +1,32 @@
+package com.hulu.excersice.binarytree.config;
+
+import java.io.FileNotFoundException;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class)
+public class PropertyTests {
+    
+	@Value("${root.nodes}")
+    private String[] nodes;
+
+    @Configuration
+    @Import({PropertyTestConfiguration.class})
+    static class ContextConfiguration {
+    }
+    
+    @Test
+    public void loadNodesFromPropertiesSourceTest() {
+        Assert.assertNotNull(nodes);
+    }
+}
