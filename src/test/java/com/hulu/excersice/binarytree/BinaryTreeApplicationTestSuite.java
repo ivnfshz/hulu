3bf908d9a5b71d454c9824d8cd4f73f9bca59704
ivnfshz12 <jivanfloresh@gmail.com>
2018-06-22 00:30:11 -0500

    add test suite class to load a bunch of tests

---
+++ src/test/java/com/hulu/excersice/binarytree/BinaryTreeApplicationTestSuite.java
@@ -0,0 +1,15 @@
+package com.hulu.excersice.binarytree;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import com.hulu.excersice.binarytree.service.BinaryTreeServiceTests;
+
+@RunWith(Suite.class)
+@SuiteClasses({ 
+	BinaryTreeServiceTests.class
+	})
+public class BinaryTreeApplicationTestSuite {
+
+}
